e2cfd799da29f1ca22d2f82902f216d8905e7795
Jessie S <jessie.sholler@gmail.com>
2019-10-03 18:18:03 -0400

    best match is working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -30,23 +30,40 @@ module.exports = (app) => {
 
         var compareScores = [];
         // Best match 
-        // var match = 0; 
+        var bestMatch; 
+       
 
         // Loops through current pawfiles
         for (var i = 0; i < friends.length; i++){
             var totalDifference = 0;
+            var minDifference = -1;
             // Loops through currentScore array
             for (x = 0; x < currentScore.length; x++){
                 // Math.abs calculates the results as positive numbers 
                 totalDifference += Math.abs(currentScore[x] - friends[i].scores[x]);
             }
+            if (minDifference < 0 || totalDifference < minDifference ){
+                minDifference = totalDifference
+                bestMatch = friends[i].first_name; 
+            }
             // Pushes the difference between each member and pushes the results to the compare scores array 
-            compareScores.push(totalDifference);
-            console.log(totalDifference)
+            // compareScores.push(totalDifference);
+            //  compareScores[i] = totalDifference;
+           
         }
+        // var match = compareScores[0];
 
+        // for (var i = 1; i < compareScores.length; i++){
+           
+        //     if(compareScores[i] <= match){
+        //         match = compareScores[i]
+        //         bestMatch = i;
+        //         console.log("This is the best match: " + bestMatch)
+        //     }
+        // }
+        console.log("Total Differences: " + bestMatch)
         friends.push(newPawfile)
-        res.json(newPawfile);
+        res.json(newPawfile[bestMatch]);
     
     })
     // console.log(friends.length)
